d7d5eec7ecdbbe50390ef3b4d6cf4c5085472299
Borcsa <borisz@juhasz.dev>
2026-03-04 23:06:29 +0100

    feat: add base project with content module

---
+++ app/pages/content/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { data: base } = await useAsyncData(() => queryCollection('content').path('/base').first())
+
+</script>
+
+<template>
+  <ContentRenderer v-if="base" :value="base" />
+  <div v-else>Home not found</div>
+</template>
