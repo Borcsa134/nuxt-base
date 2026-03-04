5237de0b2a8cd97466e04d217b0d64079b69893b
Borcsa <borisz@juhasz.dev>
2026-03-04 23:15:44 +0100

    feat: add vitest

---
+++ test/unit/base.spec.ts
@@ -0,0 +1,3 @@
+it('smokes', () => {
+  expect(1).toBe(1);
+})
---
+++ vitest.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vitest/config'
+import { defineVitestProject } from '@nuxt/test-utils/config'
+
+export default defineConfig({
+  test: {
+    watch: false,
+    environment: 'happy-dom',
+    projects: [
+      {
+        test: {
+          name: 'unit',
+          include: ['test/unit/*.{test,spec}.ts'],
+          environment: 'node',
+          globals: true
+        },
+      },
+      {
+        test: {
+          name: 'e2e',
+          include: ['test/e2e/*.{test,spec}.ts'],
+          environment: 'node',
+          globals: true
+        },
+      },
+      await defineVitestProject({
+        test: {
+          name: 'nuxt',
+          include: ['test/nuxt/*.{test,spec}.ts'],
+          environment: 'nuxt',
+          globals: true
+        },
+      }),
+    ],
+  },
+})
